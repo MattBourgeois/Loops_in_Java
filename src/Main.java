4c8ac419985b1a771430016607a1c3cf07e9d7e6
MattBourgeois <Bourgeoismatthew6@gmail.com>
2023-06-26 16:44:34 -0400

    more for loops, while using expressions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int counter = 1; counter <= 5; counter++) {
+            System.out.println(counter);
+        }
+
+        for (double rate = 2.0; rate <= 5.0; rate++) {
+            double interestAmount = calculateInterest(10000.0, rate);
+            System.out.println("10,000 at " + rate + "% interest = " + interestAmount);
+        }
+        for(double rate = 7.5; i <=10;  i += 0.25){
+            double interestAmount = calculateInterest(100.0, i);
+            System.out.println("$100.00 " + 1 "% interest = $" + interestAmount);
+        }
     }
-}
+        public static double calculateInterest ( double amount, double interestRate){
+
+            return (amount * (interestRate / 100));
+        }
+    }
